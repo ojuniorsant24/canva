0fae8e255fe1fa868c7146e34ca8d3a327821c88
ojuniorsant24 <juniorsantosbr24@gmail.com>
2025-01-30 23:13:42 -0300

    Add files via upload

---
+++ canva/js/33e1bc113bdf5f49.pt-BR.js
@@ -0,0 +1,2 @@
+window['cmsg']['assets'] = window['cmsg']['assets'] || {};
+window['cmsg']['assets']["pt-BR"] = Object.assign(window['cmsg']['assets']["pt-BR"] || {}, {"15706":{"js":["6018f2c7a77c016c.strings.js"],"css":[]},"38701":{"js":["fcb2611b11892de4.strings.js"],"css":[]}});
